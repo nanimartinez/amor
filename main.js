b07ff8f426c6688604b98a68316e7c9b2667e813
Nani Martinez <nanimartinez05.2@gmail.com>
2024-06-01 22:27:14 -0300

    mejoras en los prompt

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 for (let i = 0; i < 1; i++) {
-    let respuesta = prompt("Mario Paez ¿Quieres ser mi amor?");
+    let respuesta = prompt("Marito Paez ¿Quieres ser mi amor? (Puedes responder por SI o por NO)");
     if (respuesta.toLowerCase() === "sí" || respuesta.toLowerCase() === "si") {
-      alert("Yo también quiero ser tu amor");
+    alert("Yo también quiero ser tu amor");
       break; // Salimos del bucle si la respuesta es sí
+    } else if (respuesta.toLowerCase() === "no") {
+      alert("Está bien, volveré a preguntar.");
+      i--; // Restamos uno al contador para que el bucle se repita
     } else {
+      alert("Por favor, responde solo con 'SI' o 'NO'.");
       i--; // Restamos uno al contador para que el bucle se repita
     }
   }
